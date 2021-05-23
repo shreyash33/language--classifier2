c8c8e5101ccf2bbce2e6cd1687e03a5640f0c112
shreyash33 <shreyashdeshpande33@gmail.com>
2021-05-23 20:37:48 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -23,7 +23,7 @@ app2.config['SECRET_KEY'] = "Shreyash"
 db = SQLAlchemy(app2)
 
 class User(db.Model):
-    __tablename__ = 'User'
+   __tablename__ = 'User'
    id = db.Column(db.Integer, primary_key = True)
    name = db.Column(db.String(100))
    password = db.Column(db.String(50))
